8c0eb617c3ecd04494676a48fc34ec206a47a465
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2022-10-12 17:00:43 +0200

    finished project, slot machine is working well

--- main.py
+++ main.py
@@ -15,6 +15,28 @@ symbol_count = {
     "D": 8,
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2,
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
@@ -88,8 +110,7 @@ def get_bet():
         
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -104,6 +125,24 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    if winnings == 0:
+        print("Désolé, vous avez perdu.")
+    else:
+        print(f"Vous avez gagné {winnings}€.")
+        print(f"Vous avez gagné sur les lignes:", *winning_lines)
+        return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Votre solde actuel est de {balance}€.")
+        answer = input("Appuyez sur entrer pour jouer (ou q pour quitter).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"Vous partez avec {balance}€.")
 
 
 main()
